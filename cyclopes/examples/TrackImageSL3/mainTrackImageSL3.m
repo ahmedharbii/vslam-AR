3ab289d62e40a183115f5b3312a55df46fe04bb7
Abdelrhman Bassiouny <Abdelrhman_bassuny@hotmail.com>
2022-03-06 21:12:14 +0100

    removed images

--- cyclopes/examples/TrackImageSL3/mainTrackImageSL3.m
+++ cyclopes/examples/TrackImageSL3/mainTrackImageSL3.m
@@ -50,7 +50,7 @@ DEBUG_LEVEL_3 = 0;
 
 % AR
 kalb = imread("dog_grayscale_p.png");
-obj = readObj("models/wolf.obj");
+obj = readObj("models/fox.obj");
 % display_obj(obj,"dog_grayscale_p.png")
 w = 632;
 h = 480;
@@ -259,3 +259,154 @@ if norm_x > tracking_param.changereference_thresh
     change = true
 end
 return
+
+% overlay foreground_image on background_image
+% where foreground is zeroes at pixels that are related to backround.
+function overlay_2_images(background_image, foreground_image)
+    [x, y] = find(foreground_image>0);
+    overlaped_image = background_image;
+    for i=1:size(x, 1)
+        overlaped_image(x(i), y(i)) = foreground_image(x(i), y(i));
+    end
+    
+    figure(4);
+    imshow(overlaped_image);
+    Filename = sprintf('kalb/test_%s.png', datestr(now,'mm-dd-yyyy HH-MM-SS'));
+    imwrite(overlaped_image,Filename);
+return;
+
+%% warp 2d image using polygon points
+function warped_2d_image = warp_2d_image(image_to_warp, background_image, polygon)
+    matchedPtsDistorted = [[0,0];[size(image_to_warp,1),0];...
+        [size(image_to_warp,1),size(image_to_warp,2)];...
+        [0,size(image_to_warp,2)]];
+    
+    matchedPtsOriginal = polygon(1:2,1:end-1)';
+    
+    [tform,inlierIdx] = estimateGeometricTransform2D(matchedPtsDistorted,matchedPtsOriginal,'projective');
+    
+    outputView = imref2d(size(background_image.I));
+    warped_2d_image = imwarp(image_to_warp,tform,'OutputView',outputView);
+return;
+
+%% find 3d projection matrix
+function result = projection_matrix(camera_parameters, homography)
+    %From the camera calibration matrix and the estimated homography
+    %compute the 3D projection matrix
+    % Compute rotation along the x and y axis as well as the translation
+
+    homography = homography * -1;
+    rot_and_transl = camera_parameters \ homography;
+    col_1 = rot_and_transl(:, 1);
+    col_2 = rot_and_transl(:, 2);
+    col_3 = rot_and_transl(:, 3);
+    % normalise vectors
+    l = sqrt(norm(col_1, 2) * norm(col_2, 2));
+    rot_1 = col_1 / l;
+    rot_2 = col_2 / l;
+    translation = col_3 / l;
+%     translation(1,1) = -1 * translation(1,1);
+%     translation(3,1) = -1 * translation(3,1);
+    % compute the orthonormal basis
+%     c = rot_1 + rot_2;
+%     p = cross(rot_1, rot_2);
+%     d = cross(c, p);
+%     rot_1 = (c / norm(c) + d / norm(d)) *  (-1 / sqrt(2));
+%     rot_2 = (c / norm(c) - d / norm(d)) *  (1 / sqrt(2));
+    rot_3 = cross(rot_1, rot_2);
+    % finally, compute the 3D projection matrix from the model to the current frame
+%     rot_x = [[rotx(30); [0, 0, 0]], [0; 0; 0; 1]];
+%     rot_y = [[roty(0); [0, 0, 0]], [0; 0; 0; 1]];
+%     rot_z = [[rotz(0); [0, 0, 0]], [0; 0; 0; 1]];
+%     rot = rot_x * rot_y * rot_z;
+    projection = [rot_1, rot_2, rot_3, translation];
+    result = camera_parameters * projection;
+    return;
+
+%% render .obj file in the image using projection matrix
+function img = render(img, obj, projection, h, w)
+    
+% Render a loaded obj model into the current video frame
+
+    vertices = obj.v;
+    scale_matrix = eye(3) * 1;
+
+    rot_x = [[rotx(90); [0, 0, 0]], [0; 0; 0; 1]];
+    rot_y = [[roty(0); [0, 0, 0]], [0; 0; 0; 1]];
+    rot_z = [[rotz(0); [0, 0, 0]], [0; 0; 0; 1]];
+    rot = rot_x * rot_y * rot_z;
+%     h = size(model,1);
+%     w = size(model,2);
+%     h = 200;
+%     w = 300;
+    
+    % find center of 3d model to use it to translate its center to patch
+    % center
+    points = [];
+    for i=1:size(obj.f.v,1)
+        face_vertices = obj.f.v(i,:);
+        for v_idx=1:size(face_vertices,2)
+            vertex = face_vertices(v_idx);
+            points = [points; vertices(vertex,:)];
+        end
+        points = points * scale_matrix;
+
+        % render model in the middle of the reference surface. To do so,
+        % model points must be displaced
+    end
+    center_x = 0.5 * (abs(min(points(:, 1))) + abs(max(points(:, 1))));
+    center_y = 0.5 * (abs(min(points(:, 2))) + abs(max(points(:, 2))));
+
+
+    for i=1:size(obj.f.v,1)
+        points = [];
+        face_vertices = obj.f.v(i,:);
+        for v_idx=1:size(face_vertices,2)
+            vertex = face_vertices(v_idx);
+            points = [points; vertices(vertex,:)];
+        end
+        points = points * scale_matrix;
+
+        % render model in the middle of the reference surface. To do so,
+        % model points must be displaced
+        tmp_points = [];
+        for p_idx=1:size(points,1)
+            p = points(p_idx,:);
+            tmp_points = [tmp_points; [p(1) + w + center_x, p(2) + h - center_y, p(3), 1]];
+        end
+        points = tmp_points;
+
+        % Prespective Transform
+        %=======================
+
+    %     tform = geometricTransform3d(project_func);
+    %     tform = projective2d(projection);
+    %     rot = projection(:,1:3)';
+    %     trans = projection(:,4)';
+    %     tform = rigid3d(single(rot), single(trans));
+    %     ptCloud1 = pointCloud(single(points));
+    %     dst = pctransform(ptCloud1, tform);
+
+        % construct the affine projection transform
+        hom_projection = [projection; [0, 0, 0, 1]];
+        tform = affine3d(hom_projection');
+        
+%         points = rot * points;
+        % apply the transform to the 3d model points
+        [U1,V1,W1] = transformPointsForward(tform,points(1,1),points(1,2), points(1,3));
+        [U2,V2,W2] = transformPointsForward(tform,points(2,1),points(2,2), points(2,3));
+        [U3,V3,W3] = transformPointsForward(tform,points(3,1),points(3,2), points(3,3));
+        dst = [
+            [U1/W1,V1/W1];
+            [U2/W2,V2/W2];
+            [U3/W3,V3/W3]];
+%         dst = hom_projection * points';
+        imgpts = int32(dst);
+
+        % fill the pixels in the image correspoinding to the 3d model
+        % projected image.
+        mask = roipoly(img,imgpts(:,1),imgpts(:,2));
+        img(mask) = 255;
+
+    end
+    return;

68133fc4be751733bdd3c465172a48ae9af36d24
Abdelrhman Bassiouny <Abdelrhman_bassuny@hotmail.com>
2022-03-06 14:24:18 +0100

    AR working

--- cyclopes/examples/TrackImageSL3/mainTrackImageSL3.m
+++ cyclopes/examples/TrackImageSL3/mainTrackImageSL3.m
@@ -50,6 +50,17 @@ DEBUG_LEVEL_3 = 0;
 
 % AR
 kalb = imread("dog_grayscale_p.png");
+obj = readObj("models/wolf.obj");
+% display_obj(obj,"dog_grayscale_p.png")
+w = 632;
+h = 480;
+a = 100 * pi / 180.0;
+d = sqrt(w^2 + h^2);
+f = 0.5 * d / tan(a/2);
+camera_parameters = [
+    [f, 0, w/2];
+    [0, f, h/2];
+    [0, 0, 1]];
 
 %for reference patch changing - Question 6
 tracking_param.changereference = 0;
@@ -135,10 +146,13 @@ for(k=capture_params.first+1:capture_params.last)
         [H(:,:,i), WarpedImage, norm_x] = TrackImageSL3(ReferenceImage, CurrentImage, Htrack, tracking_param);
 		H(:,:,i) %for displaying
 
-%         tform = projective2d(H(:,:,i)'); 
-%         Ir = imwarp(Ir_original, tform); 
         % warp a 2d image inside a polygon defined patch.
+        %         tform = projective2d(H(:,:,i)'); 
+        %         Ir = imwarp(Ir_original, tform); 
         Ir = warp_2d_image(kalb, CurrentImage, WarpedImage.polygon);
+        
+        % estimate the 3d projection matrix
+        proj_mat = projection_matrix(camera_parameters, H(:,:,i));
 
         % for changing the reference patch for Question 6
         all_x = [all_x norm_x];
@@ -157,9 +171,24 @@ for(k=capture_params.first+1:capture_params.last)
         end
 	
 		if(tracking_param.display)
-			figure(1); hold on;	
-			DrawImagePoly('Warped Current Image', 1, CurrentImage.I, WarpedImage.polygon);
+            % draw polygon, show warped image, and error image.
+			figure(1); hold on;
+            DrawImagePoly('Warped Current Image', 1, CurrentImage.I, WarpedImage.polygon);
+
+            % overlay a 2d image on the polygon defined image patch
             overlay_2_images(CurrentImage.I, Ir)
+
+            % render a 3d object on the polygon defined image patch
+            [x, y] = find(WarpedImage.I>0);
+            img = render(CurrentImage.I, obj, proj_mat, min(y), max(x));
+
+            % save the rendered image
+            Filename = sprintf('3d/test_%s.png', datestr(now,'mm-dd-yyyy HH-MM-SS'));
+            imwrite(img,Filename);
+
+            % show the rendered image
+            figure(2);
+            imshow(img);
         end
 
 end
@@ -171,16 +200,6 @@ assignin('base','change_ref_curr_img',change_ref_curr_img);
 assignin('base','change_ref_wrap_img_polygon',change_ref_wrap_img_polygon);
 
 return;
-
-
-% function to decide whether to change ref image or not
-function [change] = change_ref_or_not(norm_x, tracking_param)
-change = false;
-if norm_x > tracking_param.changereference_thresh
-    change = true
-end
-return
-
 
 % Default test function if no values are given
 function test()
@@ -200,10 +219,10 @@ tracking_params.size_x = 8; % number of parameters to estimate
 % Change for your paths here
 capture_params.homedir = [pwd '\cyclopes\']
 %for the street:
-capture_params.data_dir = [pwd '\Versailles_canyon\Right\']
+% capture_params.data_dir = [pwd '\Versailles_canyon\Right\']
 % capture_params.data_dir = [pwd '\Versailles_canyon\Left\']
 %for underwater: 
-% capture_params.data_dir = [pwd '\IMAGES_smallRGB\']
+capture_params.data_dir = [pwd '\IMAGES_smallRGB\']
 
 %capture_params.data_dir = [getenv('DIR_DATA'), '/../data/Versailles/Versailles_canyon/Left/']; 
 %capture_params.homedir = getenv('DIR_CYCLOPES'); 
@@ -218,8 +237,8 @@ capture_params.suffix = '.png';
 
 capture_params.string_size= 4; %4
 
-capture_params.first = 220; %1
-capture_params.last = 280;
+capture_params.first = 281; %1
+capture_params.last = 481;
 capture_params.savepolygon = 0; % to save the polygon --> 1
 capture_params.loadpolygon = 1; %to load the polygon --> 1
 
@@ -242,6 +261,15 @@ end
 
 return;
 
+%% Helper Functions
+%% function to decide whether to change ref image or not
+function [change] = change_ref_or_not(norm_x, tracking_param)
+change = false;
+if norm_x > tracking_param.changereference_thresh
+    change = true
+end
+return
+
 % overlay foreground_image on background_image
 % where foreground is zeroes at pixels that are related to backround.
 function overlay_2_images(background_image, foreground_image)
@@ -257,7 +285,7 @@ function overlay_2_images(background_image, foreground_image)
     imwrite(overlaped_image,Filename);
 return;
 
-% warp 2d image using polygon points
+%% warp 2d image using polygon points
 function warped_2d_image = warp_2d_image(image_to_warp, background_image, polygon)
     resized_image = imresize(image_to_warp,[100, 100]);
     matchedPtsDistorted = [[0,0];[100,0];[100,100];[0,100]];
@@ -268,3 +296,93 @@ function warped_2d_image = warp_2d_image(image_to_warp, background_image, polygo
     outputView = imref2d(size(background_image.I));
     warped_2d_image = imwarp(resized_image,tform,'OutputView',outputView);
 return;
+
+%% find 3d projection matrix
+function result = projection_matrix(camera_parameters, homography)
+    %From the camera calibration matrix and the estimated homography
+    %compute the 3D projection matrix
+    % Compute rotation along the x and y axis as well as the translation
+    homography = homography * -1;
+    rot_and_transl = camera_parameters \ homography;
+    col_1 = rot_and_transl(:, 1);
+    col_2 = rot_and_transl(:, 2);
+    col_3 = rot_and_transl(:, 3);
+    % normalise vectors
+    l = sqrt(norm(col_1) * norm(col_2));
+    rot_1 = col_1 / l;
+    rot_2 = col_2 / l;
+    translation = col_3 / l;
+    % compute the orthonormal basis
+    c = rot_1 + rot_2;
+    p = cross(rot_1, rot_2);
+    d = cross(c, p);
+    rot_1 = (c / norm(c) + d / norm(d)) *  (1 / sqrt(2));
+    rot_2 = (c / norm(c) - d / norm(d)) *  (1 / sqrt(2));
+    rot_3 = cross(rot_1, rot_2);
+    % finally, compute the 3D projection matrix from the model to the current frame
+    projection = [rot_1, rot_2, rot_3, translation];
+    result = camera_parameters * projection;
+    return;
+
+%% render .obj file in the image using projection matrix
+function img = render(img, obj, projection, h, w)
+    
+% Render a loaded obj model into the current video frame
+    
+    vertices = obj.v;
+    scale_matrix = eye(3) * 0.1;
+%     h = size(model,1);
+%     w = size(model,2);
+%     h = 200;
+%     w = 300;
+    for i=1:size(obj.f.v,1)
+        points = [];
+        face_vertices = obj.f.v(i,:);
+        for v_idx=1:size(face_vertices,2)
+            vertex = face_vertices(v_idx);
+            points = [points; vertices(vertex,:)];
+        end
+        points = points * scale_matrix;
+
+        % render model in the middle of the reference surface. To do so,
+        % model points must be displaced
+        tmp_points = [];
+        for p_idx=1:size(points,1)
+            p = points(p_idx,:);
+            tmp_points = [tmp_points; [p(1) + w, p(2) + h, p(3), 1]];
+        end
+        points = tmp_points;
+
+        % Prespective Transform
+        %=======================
+
+    %     tform = geometricTransform3d(project_func);
+    %     tform = projective2d(projection);
+    %     rot = projection(:,1:3)';
+    %     trans = projection(:,4)';
+    %     tform = rigid3d(single(rot), single(trans));
+    %     ptCloud1 = pointCloud(single(points));
+    %     dst = pctransform(ptCloud1, tform);
+
+        % construct the affine projection transform
+        hom_projection = [projection; [0, 0, 0, 1]];
+        tform = affine3d(hom_projection');
+        
+        % apply the transform to the 3d model points
+        [U1,V1,W1] = transformPointsForward(tform,points(1,1),points(1,2), points(1,3));
+        [U2,V2,W2] = transformPointsForward(tform,points(2,1),points(2,2), points(2,3));
+        [U3,V3,W3] = transformPointsForward(tform,points(3,1),points(3,2), points(3,3));
+        dst = [
+            [U1/W1,V1/W1];
+            [U2/W2,V2/W2];
+            [U3/W3,V3/W3]];
+%         dst = hom_projection * points';
+        imgpts = int32(dst);
+
+        % fill the pixels in the image correspoinding to the 3d model
+        % projected image.
+        mask = roipoly(img,imgpts(:,1),imgpts(:,2));
+        img(mask) = 255;
+
+    end
+    return;
